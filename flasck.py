315b287b36d2ed5c0e92badb9236e8ad048be2bb
计算机1808-佟懿轩-20184465 <48703739+EdmundTYX@users.noreply.github.com>
2021-05-03 13:54:55 +0800

    Update flasck.py

--- flasck.py
+++ flasck.py
@@ -5,8 +5,9 @@ import SQL_file
 
 app = Flask(__name__)
 
+# 需要初始化管理员
 
-@app.route('/get_file/data/<name>', methods=['GET'])  # <file_name>
+'''@app.route('/get_file/data/<name>', methods=['GET'])  # <file_name>
 def get_local_resource(name):  # file_name
     # print(request.args.get('name'))
     basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,23 +16,40 @@ def get_local_resource(name):  # file_name
     # basedir一般是在配置文件中
     file_path = os.path.join(basedir, 'data', name)
     print(file_path)
-    return send_file(file_path)
+    return send_file(file_path)'''
 
 
 @app.route('/update/', methods=['POST'])
 def update():
-    """更新车辆信息"""
-    global idd
+    """上架车辆信息"""
+    """存图片的过程不确定"""
     global db
     account = request.form.get('account')
     name = request.form.get('name')
     age = request.form.get('age')
+    price = request.form.get('money')
     filenum = request.form.get('filenum')
     img0 = request.files.get('file0')
     print(account)
     print(name)
     print(age)
+    print(price)
     print(filenum)
+    if db.init_s_seller(account) == '': return 'account_False'
+    if db.set_s_pay_RQ_code_url(account, '') == '': return 'pay_RQ_code_url_False'
+    ono = db.init_o_order(account, price)
+    filenum = int(filenum)  # filenum就是前端传来的图片个数
+    files = []
+    paths = []
+    for i in range(filenum):
+        par = 'file' + str(i)  # 得到对应的图片的参数名
+        files.append(request.files.get(par))  # 根据参数名获得对应的图片文件
+    for file in files:
+        file.save('data/')  # 把图片文件存在本地的filepath路径下
+        # 注意这个filename应该每张图片对应一个自己的不同的文件名，根据后端需要而自行设定
+    for path in paths:
+        # path是图片在本地的地址
+        db.upload_pic(ono, path)
     img0.save('data/img0.jpg')
     # db.insert(idd, name, age, 'data/%s.jpg' % idd)
     # idd += 1
@@ -63,39 +81,44 @@ def dele():
 
 @app.route('/search/', methods=['POST'])
 def search():
+    """对于能否运行有极大疑惑"""
     # 实现根据key值来查询对应的车辆信息，key值就是买家的搜索类别
     # 这个函数被复用了，key值也可以是买家的账号，根据账号返回买家已经被后台确定的车辆信息
     # 又被复用了，key值也可以是'admin0'，表示这是管理员要看卖家提交的车辆信息
     # key值也可以是'admin1'，表示这是管理员要根据买家付款备注中的的账号以及车辆编号来
     global db
+    dicts = []
     key = request.form.get('key')
     # 车辆类型有两种 山地车、普通车
     if str.isdecimal(key):
-    # 返回买家的已经被后台确认收到款的车辆信息
-
+        # 返回买家的已经被后台确认收到款的车辆信息
+        """因为这个函数不同，所以返回格式需要改动"""
+        result = db.get_order_by_bno(key)
+        for i in result:
+            dicts.append({'no': i[0][0], 'name': i[2][0], 'age': i[2][1], 'money': i[0][5], 'pics': i[1]})
     elif key == 'admin0':
-    # 返回卖家提交的待审核的车辆信息
-
+        # 返回卖家提交的待审核的车辆信息
+        result = db.get_order_new()
     elif key == '山地车' or '普通车':
-    # 返回满足条件（（属性name）==key）的车辆信息
-
+        # 返回满足条件（（属性name）==key）的车辆信息
+        result = db.get_order_unsold_by_tag(key)
     else:
         # 返回10条未卖出的车辆信息，最好设一个计数器，每次返回下一个10条信息
-        dist = db.get_order_unsold()
-
+        result = db.get_order_unsold()
+    for i in result:
+        dicts.append({'no': i[0][0], 'name': i[2][0], 'age': i[2][1], 'money': i[0][3], 'pics': i[1]})
     # dist1是符合key类别的车辆的信息
     # dist1 = {车辆的编号(也就是订单的编号)，车辆的类别(name)属性，车辆的年龄(age)属性，车辆的价格，图片总数量，图片0链接，图片1链接，...}
     # return [dist1, dist2, ...]
-    dist1 = {'no': '1', 'name': '山地车', 'age': '3', 'money': 100,
-             'pics': ['http://localhost:5555/get_file/data/1.jpg', 'http://localhost:5555/get_file/data/10.jpg']}
-    dist2 = {'no': '2', 'name': '山地车', 'age': '3', 'money': 100,
-             'pics': ['http://localhost:5555/get_file/data/1.jpg', 'http://localhost:5555/get_file/data/10.jpg']}
-    return json.dumps([dist1, dist2])
+    # dist1 = {'no': '1', 'name': '山地车', 'age': '3', 'money': 100,
+    #          'pics': ['http://localhost:5555/get_file/data/1.jpg', 'http://localhost:5555/get_file/data/10.jpg']}
+    # dist2 = {'no': '2', 'name': '山地车', 'age': '3', 'money': 100,
+    #          'pics': ['http://localhost:5555/get_file/data/1.jpg', 'http://localhost:5555/get_file/data/10.jpg']}
+    return json.dumps(dicts)
 
 
 """@app.route('/signup/', methods=['POST'])
 def signup():
-    global idd
     global db
     account = request.form.get('account')
     name = request.form.get('name')
@@ -108,18 +131,20 @@ def signup():
     else:
         return 'sign_up_True'"""
 
-
-@app.route('/login/', methods=['POST'])
+'''@app.route('/login/', methods=['POST'])
 def login():
-    global idd
     global db
     account = request.form.get('account')
     password = request.form.get('password')
     if db.get_password(account) is None:
         return 'login_error:no such user'
     if password == db.get_password(account):
-        return {'right': '1', 'admin': '1'}
-
+        if db.get_if_admin(account):
+            return {'right': '1', 'admin': '1'}
+        else:
+            return {'right': '1', 'admin': '0'}
+    else:
+        return {'right': '0', 'admin': '0'}'''
 
 '''@app.route('/money/', methods=['GET'])
 def money():
@@ -129,7 +154,7 @@ def money():
 
 @app.route('/confirm/', methods=['POST'])
 def confirm():
-    global idd
+    """过程不确定"""
     global db
     account = request.form.get('account')
     bike_num = request.form.get('bike_num')
@@ -146,5 +171,4 @@ if __name__ == '__main__':
     basedir = os.path.abspath(os.path.dirname(__file__))
     print(basedir)
     db = SQL_file.SQL()
-    idd = 10
     app.run(host='0.0.0.0', port=5555)  # ,debug=True
